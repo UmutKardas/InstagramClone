1bca76f8b2a0950aa89c14fb164ed503872e83ed
Hüseyin Umut Kardaş <kardasumt@gmail.com>
2024-08-18 17:21:26 +0300

    Add firebase pods

--- InstagramClone/InstagramCloneApp.swift
+++ InstagramClone/InstagramCloneApp.swift
@@ -5,10 +5,19 @@
 //  Created by Hüseyin Umut Kardaş on 18.08.2024.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct InstagramCloneApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
